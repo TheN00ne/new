2b1973e60028fa013f6547871d3d452385c48cd8
Noone <ilyind777@gmail.com>
2024-07-10 22:32:26 +0300

    second

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("JS is working!");
+const nameInp = document.getElementById("name");
+const passwordInp = document.getElementById("password");
+const btn = document.getElementById("btn");
+
+scale = (e) => {
+ e.target.style.transform = "scale(1.01)";
+ setTimeout(() => {e.target.style.transform = "scale(1)"}, 200)
+}
+
+nameInp.addEventListener("input", scale);
+passwordInp.addEventListener("input", scale);
+
+passwordInp.addEventListener("keydown", (e) => {
+    if(e.code == "ControlLeft"){
+        passwordInp.setAttribute("type", "text");
+    }
+})
+
+passwordInp.addEventListener("keyup", () => {
+    passwordInp.setAttribute("type", "password");
+})
