a2b338a7fd10a590caea8b2cd48602adf6eea78b
Noone <ilyind777@gmail.com>
2024-07-10 23:10:12 +0300

    counter

--- script.js
+++ script.js
@@ -19,3 +19,19 @@ passwordInp.addEventListener("keydown", (e) => {
 passwordInp.addEventListener("keyup", () => {
     passwordInp.setAttribute("type", "password");
 })
+
+let counter = (start) => {
+    let count = start;
+
+    return () => {
+        let div = document.createElement("div");
+        div.style.width = "100px";
+        div.style.height = "20px";
+        div.style.backgroundColor = "red";
+        div.innerHTML = `Counter: ${count}`;
+        document.body.append(div);
+    }
+}
+
+let count1 = counter(0);
+let count2 = counter(100);
